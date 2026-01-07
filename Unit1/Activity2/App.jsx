fc9bbbf8eb459e706278d1ce0bf08306ff1ca1d0
eeddy <eeddy@unb.ca>
2026-01-07 15:03:20 -0400

    Added template code for activity 2

---
+++ Unit1/Activity2/App.jsx
@@ -0,0 +1,15 @@
+import Form from "./components/Form"
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <h1> SWE4213 Activity 2: </h1>
+      {/* TODO: Insert the form here */}
+
+    </>
+  )
+}
+
+export default App
---
+++ Unit1/Activity2/MyButton.jsx
@@ -0,0 +1,6 @@
+
+export default function MyButton({ onClick }) {
+  return (
+    // Create button component with the correct onClick that says "Submit Form"
+  );
+}
